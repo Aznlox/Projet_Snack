62cbee35dee3d064b9b5eae38be764af878c9615
dauwemarc <56584311+dauwemarc@users.noreply.github.com>
2019-12-09 10:01:25 +0100

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -18,17 +18,11 @@
       </div>
       <ul class="nav navbar-nav">
         <li class="active"><a href="Accueil.html">Accueil</a></li>
-        <li><a href="Information.html">Information</a></li>
+          <li><a href="Information.html">Info.</a></li>
       </ul>
     </div>
   </nav>
   <body>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
     <div align="center">
     <form method="post" action="traitement-accueil.php">
     <input type="submit" value="se connecter">
@@ -40,11 +34,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-
     <link rel="stylesheet" href="css/bootstrap.css">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
           <ol class="carousel-indicators">
@@ -54,21 +45,21 @@
           </ol>
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img class="d-block w-100" src="panini1.png" alt="First slide">
+              <img class="d-block w-100" src="panini1.jpg" alt="First slide">
               <div class="carousel-caption d-none d-md-block">
                 <h5></h5>
                 <p></p>
               </div>
             </div>
             <div class="carousel-item">
-              <img class="d-block w-100" src="salade2.png" alt="Second slide">
+              <img class="d-block w-100" src="salade2.jfif" alt="Second slide">
               <div class="carousel-caption d-none d-md-block">
                 <h5></h5>
                 <p></p>
               </div>
             </div>
             <div class="carousel-item">
-              <img class="d-block w-100" src="" alt="Third slide">
+              <img class="d-block w-100" src="panini2.jpg" alt="Third slide">
               <div class="carousel-caption d-none d-md-block">
                 <h5></h5>
                 <p></p>
@@ -84,8 +75,6 @@
             <span class="sr-only">Next</span>
           </a>
         </div>
-
-
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
