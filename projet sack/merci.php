37f2eae38d50ae20bfb4e3cfa0bdbac631815a5e
theo-glitch <58421051+theo-glitch@users.noreply.github.com>
2019-12-09 09:13:18 +0100

    Add files via upload

---
+++ projet sack/merci.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="staile.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+
+  <body>
+    <CENTER>
+    <p> Merci de votre commande!</p>
+
+    <input class="favorite styled"
+       type="button"
+       value="Retour à l'accueil">
+     </CENTER>
+  </body>
+</html>
