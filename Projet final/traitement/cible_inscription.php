509d486f12475996e6ac97be17872e0a2c74d0d4
Loïc <loicguo@gmail.com>
2019-12-10 11:54:24 +0100

    final

--- Projet final/traitement/cible_inscription.php
+++ Projet final/traitement/cible_inscription.php
@@ -12,8 +12,8 @@ $bdd= new PDO('mysql:host=localhost;dbname=snack;charset=utf8','root','');
 catch(Exception $e){
   die('Erreur:'.$e->getMessage());
 }
-$reponse = $bdd->prepare('SELECT * FROM etudiant WHERE Nom= :Nom and Prenom= :Prenom or identifiant= :identifiant') ;  //on prepare la requete de php pour accéder aux identifiants et aux mdp dans la base de données en sql
-$reponse->execute(array('Nom'=>$Nom,'Prenom'=>$Prenom,'identifiant'=>$identifiant)); //on insère sous forme de tableau les données que l'on veut récupérer de la base
+$reponse = $bdd->prepare('SELECT * FROM etudiant WHERE Nom= :Nom and Prenom= :Prenom and :mail or identifiant= :identifiant') ;  //on prepare la requete de php pour accéder aux identifiants et aux mdp dans la base de données en sql
+$reponse->execute(array('Nom'=>$Nom,'Prenom'=>$Prenom, 'mail'=>$mail,'identifiant'=>$identifiant)); //on insère sous forme de tableau les données que l'on veut récupérer de la base
 $donne = $reponse->fetch(); //on execute finalement la requete
 if($donne)
  {
