a7060b2e85f6e1d68472c553bf7def37cd95a1ed
Loïc <loicguo@gmail.com>
2019-12-11 12:16:58 +0100

    accueil comment

--- Projet final/vu/merci.php
+++ Projet final/vu/merci.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <link rel="stylesheet" href="../lib/css/style_merci.css">
-    <meta charset="utf-8">
+    <link rel="stylesheet" href="../lib/css/style_merci.css"><!--lien vers une page css -->
+    <meta charset="utf-8"><!--moteur utf-8 -->
     <title></title>
   </head>
-
   <body>
-    <CENTER>
-    <p> Merci de votre commande!</p>
-
+    <CENTER><!--toutes les balises dans cette balise sont centrés -->
+    <p> Merci de votre commande!</p><!--paragraphe -->
     <input class="favorite styled"
        type="button"
-       value="Retour à l'accueil" onclick="window.location.href='accueil.php'">
-     </CENTER>
-  </body>
+       value="Retour à l'accueil" onclick="window.location.href='accueil.php'"><!--bouton avec lien quand on click -->
+     </CENTER><!--fermeture de la balise de centrage -->
+  </body><!--fermeture de la balise body -->
 </html>
