38ada43983d8acc8b439d083366db219fb616456
Loïc <loicguo@gmail.com>
2019-12-20 19:26:28 +0100

    image page d'accueil

--- Projet final/index.php
+++ Projet final/index.php
@@ -61,7 +61,7 @@
     <!-- fin de la barre de navigation -->
     <!-- carousel + boutons connexion + inscription-->
     <section class="home-slider owl-carousel js-fullheight">
-      <div class="slider-item js-fullheight" style="background-image: url(lib/images/bg_1.jpg);">
+      <div class="slider-item js-fullheight" style="background-image: url(lib/images/bg1.jpg);">
       	<div class="overlay"></div>
         <div class="container">
           <div class="row slider-text js-fullheight justify-content-center align-items-center" data-scrollax-parent="true">
@@ -75,13 +75,13 @@
         </div>
       </div>
 
-      <div class="slider-item js-fullheight" style="background-image: url(lib/images/bg_2.jpg);">
+      <div class="slider-item js-fullheight" style="background-image: url(lib/images/bg2.jpg);">
       	<div class="overlay"></div>
         <div class="container">
           <div class="row slider-text js-fullheight justify-content-center align-items-center" data-scrollax-parent="true">
 
             <div class="col-md-12 col-sm-12 text-center ftco-animate">
-              <h1 class="mb-4 mt-5">Une grande variété de Panini</h1>
+              <h1 class="mb-4 mt-5">Une grande variété de Menu</h1>
               <p><a href="view/formulaire_connexion_etudiant.php" class="btn btn-info p-3 px-xl-4 py-xl-3">Connexion</a> <a href="view/inscription.php" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">Inscription</a></p>
             </div>
 
@@ -89,7 +89,7 @@
         </div>
       </div>
 
-      <div class="slider-item js-fullheight" style="background-image: url(lib/images/bg_3.jpg);">
+      <div class="slider-item js-fullheight" style="background-image: url(lib/images/bg3.jpg);">
       	<div class="overlay"></div>
         <div class="container">
           <div class="row slider-text js-fullheight justify-content-center align-items-center" data-scrollax-parent="true">
@@ -106,7 +106,7 @@
     <!-- fin du carousel + boutons connexion + inscription-->
 
     <footer class="ftco-footer ftco-bg-dark ftco-section">
-      
+
         <!-- place le copyright -->
         <div class="row">
           <div class="col-md-12 text-center">
